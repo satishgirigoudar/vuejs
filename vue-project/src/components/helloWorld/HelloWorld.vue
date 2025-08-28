1fc1f123135d54bde30221e93299f4306e525349
satishgirigoudar <satishgirigoudar516@gmail.com>
2025-08-28 18:56:21 +0530

    v-bind id and disabled

--- vue-project/src/components/helloWorld/HelloWorld.vue
+++ vue-project/src/components/helloWorld/HelloWorld.vue
@@ -1,6 +1,10 @@
 <template>
-  <h1>hello {{ name }} {{ lastName}}</h1>
+  <h2>hello {{ name }}</h2>
   <h2 v-text="name"></h2>
+  <h2 v-html="lastName"></h2>
+  <h2 v-html="hack"></h2>
+  <h2 v-bind:id="heading">Heading</h2>
+  <button v-bind="{ disabled : isDisabled }" @click="showAlert">click me</button>
 </template>
 
 <script>
@@ -8,7 +12,15 @@ export default {
   data() {
     return {
       name : 'satish',
-      lastName : 'Girigoudar',
+      lastName : '<b>Girigoudar</b>',
+      hack : `<a href="#" onclick="alert('you have hacked')">win price pool</a>`,
+      heading : 'Heading',
+      isDisabled : false,
+    }
+  },
+  methods: {
+    showAlert() {
+      alert('you clicked button');
     }
   }
 }
